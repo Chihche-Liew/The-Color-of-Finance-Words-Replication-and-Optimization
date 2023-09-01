843264806f2e90a68a656223b0e516e12c4f4eb2
Zhizhe Liu <142591189+Chihche-Liew@users.noreply.github.com>
2023-09-01 15:19:22 +0800

    Update BertShap.py
    Added a partition operation to the original code, and modified the mode of cutting strings - keeping word integrity

--- BertShap.py
+++ BertShap.py
@@ -1,14 +1,17 @@
 import torch
 import pandas as pd
-from transformers import BertForSequenceClassification, BertTokenizer
+from transformers import BertForSequenceClassification, BertTokenizerFast
 import shap
-#%%
+import re
+import nltk
+from nltk.corpus import stopwords
+
+
 device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 model = BertForSequenceClassification.from_pretrained("bert-base-uncased", num_labels=1)
 model.load_state_dict(torch.load('./bert_regression_conversation.pth'), False)
 model.to(device)
-
-tokenizer = BertTokenizer.from_pretrained("bert-base-uncased")
+tokenizer = BertTokenizerFast.from_pretrained("bert-base-uncased")
 
 
 class WrappedModel:
@@ -38,12 +41,47 @@ class WrappedModel:
 
 wrapped_model = WrappedModel(model)
 
-
-def split_text(text, chunk_size):
-    return [text[i: i + chunk_size] for i in range(0, len(text), chunk_size)]
+data = pd.read_pickle('./data/con_data.pkl', compression='zip')
 
 
-data = pd.read_csv('./data/con_data.csv')
+def preprocess_text(x):
+    x = x.lower()
+
+    x = re.sub(r'[^\w\s]', '', x)
+
+    words = nltk.word_tokenize(x)
+
+    stop_words = set(stopwords.words("english"))
+    words = [word for word in words if word not in stop_words]
+
+    processed_text = " ".join(words)
+
+    return processed_text
+
+
+data['transcript'] = data['transcript'].apply(preprocess_text)
+
+
+def split_text(text, max_length):
+    words = text.split()
+    chunks = []
+    current_chunk = ""
+
+    for word in words:
+        if len(current_chunk) + len(word) + 1 <= max_length:
+            if current_chunk:
+                current_chunk += " "
+            current_chunk += word
+        else:
+            chunks.append(current_chunk)
+            current_chunk = word
+
+    if current_chunk:
+        chunks.append(current_chunk)
+
+    return chunks
+
+
 chunk_size = 256
 new_rows = []
 for index, row in data.iterrows():
@@ -54,15 +92,25 @@ for index, row in data.iterrows():
         new_rows.append(new_row)
 
 data = pd.DataFrame(new_rows)
-transcript_texts = data['transcript'].tolist()
 del chunk_size, new_rows
 
-explainer = shap.Explainer(wrapped_model.predict, masker=shap.maskers.Text(tokenizer))
+importance = pd.DataFrame(columns=['word', 'importance'])
 
-shap_values = explainer(transcript_texts)
+explainer = shap.Explainer(wrapped_model.predict,
+                           masker=shap.maskers.Text(tokenizer))
 
-importance = pd.DataFrame([shap_values[0].data, shap_values[0].values]).T.rename({0: 'word', 1: 'importance'}, axis=1)
-importance['importance'] = importance['importance'].apply(lambda x: x[0])
-importance = importance.loc[importance['word'] != '']
+transcript_texts = data['transcript'].iloc[:].tolist()
 
-importance.to_csv('./word_importance_bert.csv')
+num_parts = 10
+part_size = len(transcript_texts) // num_parts
+transcript_parts = [transcript_texts[i: i + part_size]
+                    for i in range(0, len(transcript_texts), part_size)]
+
+
+for i, part in enumerate(transcript_parts):
+    shap_values = explainer(part)
+    df = pd.DataFrame([shap_values[0].data, shap_values[0].values]).T.rename({0: 'word', 1: 'importance'}, axis=1)
+    df['importance'] = df['importance'].apply(lambda x: x[0])
+    df = df[df['word'] != '']
+    df.to_csv('./word_importance_x_' + str(i) + '.csv')
+    importance = pd.concat([importance, df], ignore_index=True)
